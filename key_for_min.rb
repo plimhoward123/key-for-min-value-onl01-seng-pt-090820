f068a799e8e0c769bbcbcc9d4edf7ea9e317b40d
Plimon Howard V <plimon.howard@gmail.com>
2020-09-21 17:36:06 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,10 +6,12 @@ def key_for_min_value(name_hash)
   minkey = nil
 
   name_hash.each do |key,value|
+    puts value
     if (value < minval)
       minval = value
       minkey = key
     end
   end
+  
   puts minkey
 end
