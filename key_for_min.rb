5265313264e7681411db620f2b8f6512eb78388b
Plimon Howard V <plimon.howard@gmail.com>
2020-09-21 17:37:04 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  minval = 9999999
+  minval = 0
   minkey = nil
 
   name_hash.each do |key,value|
